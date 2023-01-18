7554db448e460512912e4143af17f592b11464b9
Mark-desing <mark.muijen2@gmail.com>
2023-01-18 12:00:47 +0100

    beter interactie

--- interactie/main.js
+++ interactie/main.js
@@ -3,6 +3,14 @@ window.onload = () =>{
     const shine = document.getElementsByClassName("shine")
     const shower = document.getElementsByClassName("show")
     let bord = document.getElementsByClassName("bord")
+    const vindtext = document.getElementsByClassName("vindtext")
+    let vindtextid = -1;
+
+    const sleep = async (milliseconds) => {
+        await new Promise(resolve => {
+            return setTimeout(resolve, milliseconds)
+        });
+    };
 
     const vinden = document.getElementsByClassName("vind")
 
@@ -12,7 +20,7 @@ window.onload = () =>{
 
         show.onclick = (event) =>{
             console.log("klik");
-            bord[0].object3D.position.set(0, 2, -13.4);
+            bord[0].object3D.position.set(0, 2, -11.8);
             console.log(bord[0]);
         }
     }
@@ -25,15 +33,24 @@ window.onload = () =>{
             console.log(vind)
             console.log(vind.id);
             shine[vind.id].setAttribute("opacity", 0.5);
-            bord[0].object3D.position.set(0, -6, -13.4);
+            bord[0].object3D.position.set(0, -6, -12.4);
+            vindtextid = (i);
+            console.log(vindtextid);
         }
     }
 
     for (let i = 0; i < kasten.length; i++){
         const kast = kasten[i];
 
-        kast.onclick = (event) =>{
+        kast.onclick = async (event) =>{
             shine[i].setAttribute("opacity", 0);
+            console.log(i);
+            console.log(vindtextid);
+        if (vindtextid == i) {
+            vindtext[0].object3D.position.set(0, 2, -3);
+            await sleep(5000);
+            vindtext[0].object3D.position.set(0, 2, 3);
+        } 
 
         }
     }
